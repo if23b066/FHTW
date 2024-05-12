4147e76d8e3606149d1c079f57ba7c198b09762e
Armin Naseri <if23b066@technikum-wien.at>
2024-05-12 16:06:07 +0200

    register component angelegt

--- 2. Semester/Web Frameworks/Präsenz/my-app/src/app/app.component.ts
+++ 2. Semester/Web Frameworks/Präsenz/my-app/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { RegistrationComponent } from './registration/registration.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent],
+  imports: [RouterOutlet, LoginComponent, RegistrationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'FHTW Puzzle Game';
 }
---
+++ 2. Semester/Web Frameworks/Präsenz/my-app/src/app/registration/registration.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import {
+  FormControl,
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { merge } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+
+@Component({
+  selector: 'app-registration',
+  standalone: true,
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSelectModule,
+  ],
+  templateUrl: './registration.component.html',
+  styleUrl: './registration.component.scss',
+})
+export class RegistrationComponent {
+  email = new FormControl('', [Validators.required, Validators.email]);
+  company = new FormControl('FH Technikum Wien');
+
+  errorMessage = '';
+
+  constructor() {
+    merge(this.email.statusChanges, this.email.valueChanges)
+      .pipe(takeUntilDestroyed())
+      .subscribe(() => this.updateErrorMessage());
+  }
+
+  updateErrorMessage() {
+    if (this.email.hasError('required')) {
+      this.errorMessage = 'You must enter a value';
+    } else if (this.email.hasError('email')) {
+      this.errorMessage = 'Not a valid email';
+    } else {
+      this.errorMessage = '';
+    }
+  }
+  hide = true;
+}
