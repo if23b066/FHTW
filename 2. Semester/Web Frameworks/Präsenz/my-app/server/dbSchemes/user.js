e7fdf083a7b19a39467b63a6044c839fe5439b05
Armin Naseri <if23b066@technikum-wien.at>
2024-06-12 12:00:58 +0200

    bugfix

--- 2. Semester/Web Frameworks/Präsenz/my-app/server/dbSchemes/user.js
+++ 2. Semester/Web Frameworks/Präsenz/my-app/server/dbSchemes/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 8 },
-    company: { type: String, requireyyd: true },
+    company: { type: String, required: true },
     street: { type: String, required: true },
     plz: { type: String, required: true },
     city: { type: String, required: true }
